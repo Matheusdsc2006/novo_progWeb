00fd028cf7ae4f4c95b09ee90563d77c2009be50
Matheusdsc2006 <matheusdosantoscosta55@gmail.com>
2024-07-12 10:34:39 -0300

    Cap 04

---
+++ Loja/loja/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+
+
+# Register your models here.
+from .models import * #imporata nossos models
+
+class CategoriaAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+    search_fields = ('Categoria',)
+
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+    search_fields = ('Fabricante',)
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
+    exclude = ('msgPromocao',)
+
+admin.site.register(Fabricante, FabricanteAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+
