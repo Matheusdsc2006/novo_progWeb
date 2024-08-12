082fe2f30930b47035f79e0a7c606952e839a129
Matheusdsc2006 <matheusdosantoscosta55@gmail.com>
2024-08-12 08:21:44 -0300

    12/08

--- Loja/loja/views/HomeView.py
+++ Loja/loja/views/HomeView.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from loja.models import Produto
 
 def home_view(request):
-    return render(request, template_name='home/home.html', status=200)
+    # lê a caixa de texto de produtos pesquisados
+    produto = request.GET.get("produto")
+    #filtra no banco de dados os produtos pesquisados pelo usuário
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    # inserir resultado no contexto
+    context = {
+        'produtos': produtos
+    }
+    # envia o contexto para o template
+    return render(request, template_name='home/home.html', context=context, status=200)
