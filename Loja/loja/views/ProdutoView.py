6a59f266bfb2fb024bdd415672e1f51dc2f9a45e
Matheusdsc2006 <matheusdosantoscosta55@gmail.com>
2024-09-06 09:58:36 -0300

    qualquer nome

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -9,13 +9,20 @@ from django.core.files.storage import FileSystemStorage
 
 
 def create_produto_view(request, id=None):
-    # Processa o post back gerado pela action
+    Fabricantes = Fabricante.objects.all()
+    Categorias = Categoria.objects.all()
+    context = { 
+        'fabricantes': Fabricantes,
+        'categorias': Categorias,
+    }
     if request.method == 'POST':
         produto = request.POST.get("Produto")
         destaque = request.POST.get("destaque")
         promocao = request.POST.get("promocao")
         msgPromocao = request.POST.get("msgPromocao")
         preco = request.POST.get("preco")
+        categoria = request.POST.get("CategoriaFk")
+        fabricante = request.POST.get("FabricanteFk")
         image = request.POST.get("image")
         print("postback-create")
         print(produto)
@@ -29,6 +36,8 @@ def create_produto_view(request, id=None):
             obj_produto.Produto = produto
             obj_produto.destaque = (destaque is not None)
             obj_produto.promocao = (promocao is not None)
+            obj_produto.fabricante = Fabricante.objects.filter(id=fabricante).first()
+            obj_produto.categoria = Categoria.objects.filter(id=categoria).first()
             if msgPromocao is not None:
                 obj_produto.msgPromocao = msgPromocao
             obj_produto.preco = 0
@@ -36,7 +45,6 @@ def create_produto_view(request, id=None):
                 obj_produto.preco = preco
             obj_produto.criado_em = timezone.now()
             obj_produto.alterado_em = obj_produto.criado_em
-            # Se for anexado arquivo, salva na pasta e guarda nome no objeto
             if request.FILES is not None:
                 num_files = len(request.FILES.getlist('image'))
                 if num_files > 0:
@@ -46,11 +54,13 @@ def create_produto_view(request, id=None):
                     filename = fs.save(imagefile.name, imagefile)
                     if (filename is not None) and (filename != ""):
                         obj_produto.image = filename
-            obj_produto.save()
+                obj_produto.save()
             print("Produto %s salvo com sucesso" % produto)
         except Exception as e:
             print("Erro inserindo produto: %s" % e)
-    return render(request, template_name='produto/produto-create.html',status=200)
+        return redirect("/produto")
+        
+    return render(request,  template_name='produto/produto-create.html', context=context, status=200)
 
 # adicione a função que trata o postback da interface de exclusão
 def delete_produto_postback(request, id=None):
@@ -160,11 +170,6 @@ def list_produto_view(request, id=None):
     # Adicione para definir o contexto e carregar o template
     context = {'produtos': produtos}
     return render(request, template_name='produto/produto.html', context=context, status=200)
-
-# adicione a função que chama a interface de criar produto
-# no final do arquivo
-def create_produto_view(request, id=None):
-    return render(request, template_name='produto/produto-create.html',status=200)
 
     
 
